5a57894bf1ad3c2874343a05b1b1f24742b151ee
zixuan-fei <feizx1008@163.com>
2025-10-14 01:52:57 -0400

    try

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+
+
+
+const int I2C_IMU_SCL = 41;
+const int I2C_IMU_SDA = 40;
+
+void init_I2C()
+{
+    i2c_i
+}
